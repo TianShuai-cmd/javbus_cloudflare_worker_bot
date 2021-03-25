df022d72a0d99a9b9ca1d0e5eef09b487b75dc39
TianShuai-cmd <66355066+TianShuai-cmd@users.noreply.github.com>
2021-03-25 16:08:10 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const ALLOWED_GROUPS = []
-export const BOT_TOKEN = ''
-export const ROBOT_NAME = '@javbus_bot'
+export const BOT_TOKEN = '1725592646:AAE7V9vYnO-oe2x5Tmp6k3dyPxJALIRJ114'
+export const ROBOT_NAME = '@fanhaochaxunbot'
